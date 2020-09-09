14ca7ae73f8b33edf8eef129d020279573ecd631
ViNguyenHoang <hoangvi0206@gmail.com>
2020-09-09 15:55:35 +0700

    Add Django Project Ecommerce and App Store files, Create Customer, Food, Order, Ship table

---
+++ Store/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+# User
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_code = models.CharField(primary_key=True, unique=True)
+    username = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.TextField()
+    gender = models.CharField(max_length=10)
+    dateOfBirth = models.DateTimeField()
+    is_shipper = models.BooleanField(default=False)
+    def __str__(self):
+        return self.username
+
+# Food
+class Food(models.Model):
+    # Add Rating(Avg)(Like), Image, 
+    food_name = models.CharField(max_length=100, null=True)
+    price = models.FloatField()
+    rating = models.FloatField()
+    img = models.ImageField(upload_to='images')
+    def __str__(self):
+        return self.food_name
+
+# Food in cart
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
+    date_order = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100)
+
+# Food is added to cart
+class OrderFood(models.Model):
+    food_id = models.ForeignKey(Food, on_delete=models.SET_NULL)
+    order_id = models.ForeignKey(Order, on_delete=models.SET_NULL)
+    quantity = models.IntegerField(default=0)
+    date_add = models.DateTimeField(auto_now_add=True)
+
+# Ship
+class Shipping(models.Model):
+    food = models.ForeignKey(Food, on_delete=models.SET_NULL)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL)
+    address = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=100)
+    date_add = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.address
---
+++ Store/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('foodpage/', views.Food, name='Food'),
+    path('cart/', views.Cart, name='Cart'),
+    path('search/', views.Search, name='Search'),
+    path('login/', views.Login, name='Login'),
+    path('logout/', views.Logout, name='Logout'),
+    path('register/', views.Register, name='Register'),
+]
