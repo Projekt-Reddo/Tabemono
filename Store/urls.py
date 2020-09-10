852907c0b774f367bfa4350896c4ae93973c7fe0
Tran Thien Phu <hostcode0301@gmail.com>
2020-09-10 13:08:51 +0700

    find bug

--- Store/urls.py
+++ Store/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('foodpage/', views.Food, name='Food'),
-    path('cart/', views.Cart, name='Cart'),
-    path('search/', views.Search, name='Search'),
-    path('login/', views.Login, name='Login'),
-    path('logout/', views.Logout, name='Logout'),
-    path('register/', views.Register, name='Register'),
+    # path('foodpage/', views.Food, name='Food'),
+    # path('cart/', views.Cart, name='Cart'),
+    # path('search/', views.Search, name='Search'),
+    # path('login/', views.Login, name='Login'),
+    # path('logout/', views.Logout, name='Logout'),
+    # path('register/', views.Register, name='Register'),
+    path('', views.index, name='index')
 ]
