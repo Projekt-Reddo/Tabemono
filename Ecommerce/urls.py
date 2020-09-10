03b2fabafd3aeae55b870875533c704c752f3340
ViNguyenHoang <hoangvi0206@gmail.com>
2020-09-10 13:07:44 +0700

    Fix index page

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', admin.site.urls),
-    path('Store/', include('Store.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('Store.urls')),
 ]
--- Store/urls.py
+++ Store/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.Index, name="Index"),
-    path('foodpage/', views.Food, name='Food'),
-    path('cart/', views.Cart, name='Cart'),
-    path('search/', views.Search, name='Search'),
-    path('login/', views.Login, name='Login'),
-    path('logout/', views.Logout, name='Logout'),
-    path('register/', views.Register, name='Register'),
+    path('', views.Index, name="Index"),
+    # path('foodpage/', views.Food, name='Food'),
+    # path('cart/', views.Cart, name='Cart'),
+    # path('search/', views.Search, name='Search'),
+    # path('login/', views.Login, name='Login'),
+    # path('logout/', views.Logout, name='Logout'),
+    # path('register/', views.Register, name='Register'),
 ]
